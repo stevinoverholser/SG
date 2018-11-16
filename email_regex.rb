5adbf64fd81fd0e65d9e3c7dcbf8a2d9ab6ac4c5
stevinoverholser <stevin.overholser@sendgrid.com>
2018-11-16 07:07:52 -0700

    Add files via upload

---
+++ email_regex.rb
@@ -0,0 +1,57 @@
+# valid email regex
+#\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\b
+# ruby email_regex.rb filename.csv
+# 
+# file MUST be a proper CSV format, with a single first column, with "email" as only header row.
+
+
+require 'csv'
+# csv_write() Adpated from https://github.com/sendgrid/support_scripts/blob/master/webapi_scripts/v3_unsub-delete.rb 
+@csv_perm = "a"
+def csv_write(csv_file, result_array, headers)
+    CSV.open(csv_file, @csv_perm, {:force_quotes=>true}) { |csv| result_array.each { |result| csv << [result]}}
+end
+
+input = ARGV[0]
+email_array = []
+
+# move input CSV to array
+CSV.foreach(input, headers: true) do |row| 
+	email_array.push(row[0])
+end
+size = email_array.length
+puts "Total email addresses: #{size}"
+#puts email_array.inspect
+# remove any empty cells
+sizea = email_array.length
+puts "Removing nil values"
+email_array.delete(nil)
+sizeb = email_array.length
+size = sizea - sizeb
+puts "#{size} nil values removed"
+# remove duplicate email
+sizea = email_array.length
+puts "Removing duplicates"
+email_array = email_array.uniq
+sizeb = email_array.length
+size = sizea - sizeb 
+puts "#{size} duplicate emails removed"
+
+i=0
+bad_count = 0
+while (i<size-1) do
+	#puts email_array[i]
+	if !(/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\b/ =~ email_array[i])
+		puts "removing bad address: #{email_array[i].inspect}"
+		email_array.delete_at(i) 
+		bad_count = bad_count + 1
+	end
+	i=i+1
+end
+
+size = email_array.length
+puts "#{bad_count} total invalid address removed, new CSV size: #{size}"
+#puts email_array.inspect
+email_array.insert(0,"email")
+#puts email_array.inspect
+csv_write("valid_emails.csv", email_array.flatten, "email")
---
+++ send_gen.rb
@@ -0,0 +1,164 @@
+# Generate a SendGrid cURL or JSON payload based on user input
+# run "ruby send_gen.rb" and follow in consol prompts
+############
+
+# methods for adding to users clipboard
+def pbcopy(input)
+  str = input.to_s
+  IO.popen('pbcopy', 'w') { |f| f << str }
+  str
+end
+
+def pbpaste
+  `pbpaste`
+end
+
+puts "Lets build a SendGrid v3 API mail send cURL call or JSON payload!"
+
+# get a To addresses
+to = "nil"
+to_p = ""
+moreAddresses = "y"
+toCount = 0
+puts "Enter To address or a number (less than 1000) of sink addressses to generate: "
+to = gets.chomp
+toInt = to.to_i
+while (!(/\S*@\S*[.]\S*/ =~ to) && !(toInt >= 1 && toInt <= 1000)) ## add regex validation for the Int, and use that to populate a flag to then trigger later. "if it's a number, generate"/"if it's a string..."
+  puts "Invalid input, please enter valid address or integer between 1-1000: "
+  to = gets.chomp
+  toInt = to.to_i
+end 
+if (/\S*@\S*[.]\S*/ =~ to) ##use a function to validate the address format, so that you  can be more DRY, instead of the deep logic below
+  puts "To: #{to}"
+  while (moreAddresses == "y" || moreAddresses == "yes") do
+    toCount = toCount + 1
+    while !(/\S*@\S*[.]\S*/ =~ to) do
+      if (toCount > 1)
+        puts "Enter To address: "
+        to = gets.chomp
+      end 
+      if /\S*@\S*[.]\S*/ =~ to
+        puts "To: #{to}"
+      else
+        puts "Invalid address, please enter address in format name@domain.com"
+      end
+    end
+    puts "Add more To addresses? (y/n):" ##if they enter a null, move forward
+    moreAddresses = gets.chomp
+    if !(moreAddresses == "y" || moreAddresses == "n" || moreAddresses == "yes" || moreAddresses == "no")
+      puts "Please enter either 'y' or 'n'"
+      while !(moreAddresses == "y" || moreAddresses == "n" || moreAddresses == "yes" || moreAddresses == "no") do
+        puts "Add more To addresses? (y/n):"
+        moreAddresses = gets.chomp
+      end
+    end
+    # "{\"email\": \"#{to}\"}, {\"email\": \"string (required)\"}" 
+    if toCount == 1
+        to_p = "{\"email\": \"#{to}\"}"
+        to = "nil"
+    else
+      to_p = "{\"email\": \"#{to}\"} , " + to_p ##another way to do this is to add the `, ` all the way through, then just drop the last 2 chars once you're done building the string.
+      to = "nil"
+    end
+  end
+else
+  if (toInt >= 1 && toInt <= 1000)
+  i = 1
+  while (i <= toInt) do
+    if (i == 1)
+      to="testing#{i.to_s}@sink.sendgrid.net"
+      to_p = "{\"email\": \"#{to}\"}"
+        to = "nil"
+        i= i+1
+    else
+      to="testing#{i.to_s}@sink.sendgrid.net"
+      to_p = "{\"email\": \"#{to}\"} , " + to_p
+      to = "nil"
+      i = i+1
+    end
+  end
+  end
+end
+# get a From address
+from = "nil" 
+while !(/\S*@\S*[.]\S*/ =~ from) do
+  from = "default@sink.sendgrid.net"
+  puts "Enter From address: " 
+  from = gets.chomp
+  if /\S*@\S*[.]\S*/ =~ from
+    puts "From: #{from}"
+  else
+    puts "Invalid address, please enter address in format name@domain.com"
+  end
+end
+subject = "defult"
+puts "Enter Subject line: " 
+subject = gets.chomp
+puts "Subject: #{subject}"
+
+puts "Type 'now' to send now or enter EPOCH timestamp: " 
+send_at = gets.chomp
+send_at_i = send_at.to_i
+# if not "now" or time is less than now or greater than 72 hours.
+while (send_at != "now" && (send_at_i < Time.now.to_i || send_at_i >= (Time.now.to_i + 259200))) do
+  if  send_at_i >= (Time.now.to_i + 259200)
+    puts "Can not schedule more than 72 hours in advance. please enter a value less than #{(Time.now.to_i + 259200)}"
+    send_at = gets.chomp
+    send_at_i = send_at.to_i
+  else
+    if (send_at == nil)
+      puts "No input, setting send_at to now"
+      send_at="now"
+    else
+      puts "Invalid input please 'now' to send now or enter EPOCH timestamp:"
+      send_at = gets.chomp
+      send_at_i = send_at.to_i
+    end
+  end
+end
+if (send_at == "now")
+  #Build Payload
+  payload = "{\"personalizations\": [{\"to\": [#{to_p} ] } ], \"from\": {\"email\": \"#{from}\"}, \"subject\": \"#{subject}\", \"content\": [{\"type\": \"text/plain\", \"value\": \"Hello, World!\"} ] }"
+  #puts "Here is you v3 payload (already copied to your clipboard): "
+  # print payload an load to clipboard
+else
+  #Build Payload
+  payload = "{\"personalizations\": [{\"to\": [#{to_p} ] } ], \"from\": {\"email\": \"#{from}\"}, \"subject\": \"#{subject}\", \"content\": [{\"type\": \"text/plain\", \"value\": \"Hello, World!\"} ], \"send_at\": \"#{send_at}\" }"
+  #puts "Here is you v3 payload (already copied to your clipboard): "
+  # print payload an load to clipboard
+end
+
+puts "Generate a JSON payload of a full cURL call? Please input 'json' or 'curl': "
+type = gets.chomp
+while (type != "json" && type != "curl") ##try an if/else here
+  puts "Invalid input, please only enter 'json' for a JSON payload or 'curl' for a formatted cURL call: "
+  type = gets.chomp
+end
+
+if (type == "curl")
+  puts "Enter API key: "
+  token = gets.chomp
+  puts "Do you want this cURL call copied to your clipboard? (enter 'y' to copy clipboard): "
+  copy = gets.chomp
+  cURL = "curl  -v --request POST \
+    --url https://api.sendgrid.com/v3/mail/send \
+    --header 'authorization: Bearer #{token}' \
+    --header 'content-type: application/json' \
+    --data '#{payload}'"
+   puts  "Here is your v3 cURL call: " 
+  # build cURL
+  puts "#{cURL}"
+  if(copy =="y")
+    pbcopy(cURL)
+  end 
+end
+
+if (type == "json")
+  puts "Do you want this JSON payload copied to your clipboard? (enter 'y' to copy clipboard): " 
+  copy = gets.chomp
+  puts "Here is your v3 payload: " 
+  puts "#{payload}"
+  if(copy =="y")
+    pbcopy(payload)
+  end
+end
