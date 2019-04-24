57b3040fe62fda7d263d8d5fe248fd2d0c78268e
stevinoverholser <stevin.overholser@sendgrid.com>
2019-04-24 11:44:15 -0600

    Update assign_ips.rb

--- architect/assign_ips.rb
+++ architect/assign_ips.rb
@@ -1,17 +1,17 @@
-# Assign IPs to subusers
-# NOTE!!! - this is a PUT call and you need to define EVERY IP you want assigned to a subusers. Any already assigned ips not provided in input JSON will be removed
+# Create pools and add IPs on user
+
 #expects ARGV[0] of input file:
 #EXAMPLE:
-=begin 
+=begin e
 {
  "token" : "MAKO",	
- "subusers": [
+ "pools": [
     {
-      "username": "Subuser1",
+      "name": "HIGH",
       "ips": ["149.72.170.86","167.89.21.107"]
     },
     {
-      "username": "Subuser2",
+      "name": "LOW",
       "ips": ["168.245.20.151","168.245.60.69"]
     }
   ]
@@ -32,15 +32,30 @@ end
 input = JSON.parse(data)
 token = input["token"]
 
-@subuser_array = Array.new
-input["subusers"].each { |subuser| @subuser_array << OpenStruct.new(subuser) }
-@subuser_array.each do |subuser|
-#@subuser_array.each do |subuser|
-	# PUT ip array to subuser
-		payload = "{#{subuser.ips}}".sub("{","").sub("}","")
-		uri = "https://api.sendgrid.com/v3/subusers/#{subuser.username}/ips"
-		response = HTTParty.put(uri, body: payload, headers: {"Authorization" => "token #{token}", "Content-Type" => "application/json"})
+@pool_array = Array.new
+input["pools"].each { |pool| @pool_array << OpenStruct.new(pool) }
+
+@pool_array.each do |pool|
+	# create pool
+	create_pool_uri = "https://api.sendgrid.com/v3/ips/pools"
+	payload = "{\"name\": \"#{pool.name}\"}"
+	response = HTTParty.post(create_pool_uri, body: payload, headers: {"Authorization" => "token #{token}", "Content-Type" => "application/json"})
+	# response code
+	response_code = response.code.to_s
+	puts "'#{response_code}' - #{response}"
+	# add IPs to pool
+	
+	pool["ips"].each do |ips|
+		add_ip_uri = "https://api.sendgrid.com/v3/ips/pools/#{pool.name}/ips"
+		payload = "{\"ip\": \"#{ips}\"}"
+		response = HTTParty.post(add_ip_uri, body: payload, headers: {"Authorization" => "token #{token}", "Content-Type" => "application/json"})
 		# response code
 		response_code = response.code.to_s
+		if response.headers['x-ratelimit-remaining'] == "0"
+	       puts "hitting rate limit, sleeping for a few seconds, until #{response.headers['x-ratelimit-reset']}"
+	       sleep(1) until Time.now.to_i >= response.headers['x-ratelimit-reset'].to_i
+	    end
 		puts "'#{response_code}' - #{response}"
+
+	end
 end
