d5109d8140f51064ece6b00992e1efda196742b7
stevinoverholser <stevin.overholser@sendgrid.com>
2019-07-03 11:52:08 -0600

    Update pools.rb
    added support for IPs not assigned to the account

--- architect/pools.rb
+++ architect/pools.rb
@@ -55,7 +55,29 @@ input["pools"].each { |pool| @pool_array << OpenStruct.new(pool) }
 	       puts "hitting rate limit, sleeping for a few seconds, until #{response.headers['x-ratelimit-reset']}"
 	       sleep(1) until Time.now.to_i >= response.headers['x-ratelimit-reset'].to_i
 	    end
-		puts "'#{response_code}' - #{response}"
+	    if response.code == 404
+	    	# handle is IP is not active on parent
+	    	# get subuser this IP is assigned too
+	    	response = HTTParty.get("https://api.sendgrid.com/v3/ips/#{ips}", headers: {"Authorization" => "token #{token}", "Content-Type" => "application/json"})
+	    	response_json = JSON.parse(response.to_s)
+	    	#puts response_json
+	    	subuser_array = response_json["subusers"]
+	    	# get parent username
+	    	response = HTTParty.get("https://api.sendgrid.com/v3/user/username", headers: {"Authorization" => "token #{token}", "Content-Type" => "application/json"})
+	    	response_json = JSON.parse(response.to_s)
+	    	username = response_json["username"]
+	    	subuser_array = subuser_array.push(username)
+	    	#puts "HERE #{subuser_array}"
+	    	#Assign IP to parent
+	    	response = HTTParty.put("https://api.sendgrid.com/v3/ips/#{ips}", body: "{\"subusers\": #{subuser_array}, \"warmup\" : false }" , headers: {"Authorization" => "token #{token}", "Content-Type" => "application/json"})
+	    	#add IP to pool
+	    	response = HTTParty.post(add_ip_uri, body: payload, headers: {"Authorization" => "token #{token}", "Content-Type" => "application/json"})
+	    	subuser_array.pop
+	    	#puts "https://api.sendgrid.com/v3/ips/#{ips} - {\"subusers\": #{subuser_array}, \"warmup\" : false }"
+	    	#remove IP from parent
+	    	response = HTTParty.put("https://api.sendgrid.com/v3/ips/#{ips}", body: "{\"subusers\": #{subuser_array}, \"warmup\" : false }" , headers: {"Authorization" => "token #{token}", "Content-Type" => "application/json"})
+	    end
+		#puts "'#{response_code}' - #{response}"
 
 	end
 end
