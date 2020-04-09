6d46406667e0df9f1157f9ce8a57c31232d56603
stevinoverholser <stevin.overholser@sendgrid.com>
2020-04-09 10:06:51 -0700

    Update bulk_email_validation.rb

--- architect/bulk_email_validation.rb
+++ architect/bulk_email_validation.rb
@@ -1,4 +1,4 @@
-# Input a CSV of emails - outputs CSV with validation results
+# Input a CSV of emails - outputs CSV with validation results. Update {API_KEY} with Validation API key
 # ruby bulk_email_validation.rb filename.csv
 # 
 # file MUST be a proper CSV format, with a single first column, with "email" as only header row.
